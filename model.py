b2e3b51a25157fe071e0887ffe6e0d1ba8ba07f8
Po-Lung Wang <Brownwang0426@gmail.com>
2025-01-21 16:53:45 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,207 @@
+
+
+import numpy as np
+import math
+
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils.data import DataLoader, TensorDataset, Subset
+
+import csv
+
+import multiprocessing as mp
+import os
+import sys
+import copy
+import random
+import gc
+import time
+from tqdm import tqdm
+from collections import defaultdict
+
+import itertools
+
+import dill
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+
+class custom_attn(nn.Module):
+    def __init__(self, feature_size, num_heads):
+        super(custom_attn, self).__init__()
+        assert feature_size % num_heads == 0, "feature_size must be divisible by num_heads"
+        self.bias          = False
+        self.feature_size  = feature_size
+        self.num_heads     = num_heads
+        self.head_size     = feature_size // num_heads
+        self.W_q           = nn.Linear(feature_size, feature_size, bias=self.bias)
+        self.W_k           = nn.Linear(feature_size, feature_size, bias=self.bias)
+        self.W_v           = nn.Linear(feature_size, feature_size, bias=self.bias)
+        self.W_o           = nn.Linear(feature_size, feature_size, bias=self.bias)
+
+    def split_heads(self, x):
+        batch_size, sequence_size, feature_size = x.size()
+        return x.view(batch_size, sequence_size, self.num_heads, self.head_size).transpose(1, 2)
+
+    def scaled_dot_product_attention(self, Q, K, V, mask):
+        mask_1, mask_2 = mask
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.head_size ** 0.5) #  (batch_size, num_heads, sequence_size, head_size) @ (batch_size, num_heads, head_size, sequence_size ) 
+        
+        if mask_1 != None:
+            attn_scores += mask_1                                # (batch_size, num_heads, sequence_size, sequence_size) += (batch_size, 1, sequence_size, sequence_size)
+        else:
+            attn_scores += 0
+
+        if mask_2 != None:
+            attn_probs = torch.softmax(attn_scores, dim=-1) * mask_2 # (batch_size, num_heads, sequence_size, sequence_size) * (batch_size, 1, sequence_size, sequence_size)
+        else:
+            attn_probs = torch.softmax(attn_scores, dim=-1) 
+
+        output     = torch.matmul(attn_probs, V)  # (batch_size, num_heads, sequence_size, sequence_size) @ (batch_size, num_heads, sequence_size, head_size ) 
+        return output                             # (batch_size, num_heads, sequence_size, head_size)
+
+    def combine_heads(self, x):
+        batch_size, num_heads, sequence_size, head_size = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, sequence_size, self.feature_size)
+
+    def forward(self, Q, K, V, mask=None):
+        # mask Shape: (batch_size, 1, sequence_size, sequence_size)
+        # Q    Shape: (batch_size,    sequence_size, feature_size )
+        Q    = self.split_heads(self.W_q(Q))  # Shape: (batch_size, num_heads, sequence_size, head_size )
+        K    = self.split_heads(self.W_k(K))  # Shape: (batch_size, num_heads, sequence_size, head_size )
+        V    = self.split_heads(self.W_v(V))  # Shape: (batch_size, num_heads, sequence_size, head_size )
+        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+        output      = self.W_o(self.combine_heads(attn_output))
+        return output
+
+
+    
+
+class build_model(nn.Module):
+    def __init__(self,
+                 sequence_size,
+                 feature_size,
+                 num_layers,
+                 num_heads,
+                 hidden_activation,
+                 output_activation,
+                 initializer,
+                 optimizer,
+                 loss,
+                 bias,
+                 drop_rate,
+                 alpha):
+
+        super(build_model, self).__init__()
+
+        self.sequence_size        = sequence_size
+        self.feature_size         = feature_size
+        self.num_layers           = num_layers
+        self.num_heads            = num_heads
+        self.hidden_activation    = hidden_activation
+        self.output_activation    = output_activation
+        self.initializer          = initializer
+        self.optimizer            = optimizer
+        self.loss                 = loss
+        self.bias                 = bias
+        self.drop_rate            = drop_rate
+        self.alpha                = alpha
+
+        self.positional_encoding  = nn.Parameter(self.generate_positional_encoding(self.sequence_size, self.feature_size ), requires_grad=False)
+        self.transformer_layers   = \
+        nn.ModuleList([
+            nn.ModuleList([
+                custom_attn(self.feature_size, self.num_heads),
+                nn.LayerNorm(self.feature_size),
+                nn.Linear(self.feature_size, self.feature_size, bias=self.bias),
+                nn.LayerNorm(self.feature_size)
+            ])
+            for _ in range(self.num_layers)
+        ])
+        self.output_linear     = nn.Linear(self.sequence_size * self.feature_size, self.feature_size , bias=self.bias)
+
+        # Activation functions
+        self.hidden_activation = self.get_activation(self.hidden_activation)
+        self.output_activation = self.get_activation(self.output_activation)
+
+        # Initialize weights for fully connected layers
+        self.initialize_weights(self.initializer  )
+
+        # Optimizer
+        optimizers = {
+            'adam': optim.Adam,
+            'sgd': optim.SGD,
+            'rmsprop': optim.RMSprop
+        }
+        self.optimizer = optimizers[self.optimizer.lower()](self.parameters(), lr=self.alpha)
+
+        # Loss function
+        losses = {
+            'mean_squared_error': torch.nn.MSELoss(),
+            'binary_crossentropy': torch.nn.BCELoss()
+        }
+        self.loss_function = losses[self.loss .lower()]
+
+        # Loss function
+        losses = {
+            'mean_squared_error': torch.nn.MSELoss(reduction='none'),
+            'binary_crossentropy': torch.nn.BCELoss(reduction='none')
+        }
+        self.loss_function_ = losses[self.loss .lower()]
+
+
+    def forward(self, x, mask):
+        
+        h  = x + self.positional_encoding
+
+        for i, layer in enumerate(self.transformer_layers):
+            attention_layer, attention_norm_layer, fully_connected_layer, fully_connected_norm_layer = layer
+            h_ = attention_layer(h, h, h, mask)
+            h  = attention_norm_layer(h + h_)
+            h_ = fully_connected_layer(h)
+            h  = fully_connected_norm_layer(h + h_)
+
+        h  = h.view(h.size(0), -1)
+        o  = self.output_linear(h)   
+
+        return o
+
+
+    def generate_positional_encoding(self, sequence_size, feature_size):
+        pe = torch.zeros(sequence_size,feature_size)
+        for pos in range(sequence_size):
+            for i in range(0,feature_size,2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/feature_size)))
+                if i + 1 < feature_size:
+                    pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * i)/feature_size)))
+        return pe.unsqueeze(0)  # Shape: (1, sequence_size, feature_size)
+
+    def get_activation(self,  activation):
+        activations = {
+            'relu': nn.ReLU(),
+            'leaky_relu': nn.LeakyReLU(),
+            'sigmoid': nn.Sigmoid(),
+            'tanh': nn.Tanh()
+        }
+        return activations[ activation.lower()]
+
+    def initialize_weights(self, initializer):
+        initializers = {
+            'random_uniform': nn.init.uniform_,
+            'random_normal': nn.init.normal_,
+            'glorot_uniform': nn.init.xavier_uniform_,
+            'glorot_normal': nn.init.xavier_normal_,
+            'xavier_uniform': nn.init.xavier_uniform_,
+            'xavier_normal': nn.init.xavier_normal_
+        }
+        initializer = initializers[initializer.lower()]
+        for layer in self.children():
+            if isinstance(layer, nn.Linear):
+                initializer(layer.weight)
